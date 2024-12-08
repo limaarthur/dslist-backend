84055ff1a45291c2e667c8ea0caf967fc65627bd
Arthur Lima <artglima.dev@gmail.com>
2024-12-07 22:41:48 -0300

    feat: creation of game search by id

--- src/main/java/com/ignitec/dslist/controllers/GameController.java
+++ src/main/java/com/ignitec/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.ignitec.dslist.controllers;
 
+import com.ignitec.dslist.dtos.GameDTO;
 import com.ignitec.dslist.dtos.GameMinDTO;
 import com.ignitec.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO gameDTO = gameService.findById(id);
+        return gameDTO;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
