ced92bad15d34e57e6694d24e9d8305996262cac
Arthur Lima <artglima.dev@gmail.com>
2024-12-08 20:34:44 -0300

    feat: GET method listGames by id created in the GameListController

--- src/main/java/com/ignitec/dslist/controllers/GameListController.java
+++ src/main/java/com/ignitec/dslist/controllers/GameListController.java
@@ -1,9 +1,12 @@
 package com.ignitec.dslist.controllers;
 
 import com.ignitec.dslist.dtos.GameListDTO;
+import com.ignitec.dslist.dtos.GameMinDTO;
 import com.ignitec.dslist.services.GameListService;
+import com.ignitec.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +19,18 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll() {
         List<GameListDTO> gameListDTO = gameListService.findAll();
+        return gameListDTO;
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId) {
+        List<GameMinDTO> gameListDTO = gameService.findByList(listId);
         return gameListDTO;
     }
 }
