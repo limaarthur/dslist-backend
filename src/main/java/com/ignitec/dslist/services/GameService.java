4f9b5b13f390ee4742f58908f3a5d66617986d47
Arthur Lima <artglima.dev@gmail.com>
2024-12-07 22:39:20 -0300

    feat: creation GameDTO and GameService

--- src/main/java/com/ignitec/dslist/services/GameService.java
+++ src/main/java/com/ignitec/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.ignitec.dslist.services;
 
+import com.ignitec.dslist.dtos.GameDTO;
 import com.ignitec.dslist.dtos.GameMinDTO;
 import com.ignitec.dslist.entities.Game;
 import com.ignitec.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game gameId = gameRepository.findById(id).get();
+        GameDTO gameDTO = new GameDTO(gameId);
+        return gameDTO;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> gameList = gameRepository.findAll();
         List<GameMinDTO> gameListDTO = gameList.stream().map(game -> new GameMinDTO(game)).toList();
